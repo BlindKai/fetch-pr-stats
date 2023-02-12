b77ffd7ec173fc20751e445ffb718fcc42e081b5
Blind Kai <blindkai1426@gmail.com>
2023-02-12 11:25:41 +0200

    feat: types improved, `userId` added

--- fetching.ts
+++ fetching.ts
@@ -36,23 +36,48 @@ export async function fetchReviews(prId: number) {
 function mapPullRequest(
   pr: Record<string, any>,
   reviews: Record<string, any>[]
-) {
+): MappedPullRequest {
   return {
     id: pr.id,
     url: pr.url,
     title: pr.title,
     state: pr.state,
+    userId: pr.user.id,
     user: pr.user.login,
-    createdAt: pr.created_at,
-    updatedAt: pr.updated_at,
-    closedAt: pr.closed_at,
-    mergedAt: pr.merged_at,
+    createdAt: new Date(pr.created_at),
+    updatedAt: pr.updated_at ? new Date(pr.updated_at) : null,
+    closedAt: pr.closed_at ? new Date(pr.closed_at) : null,
+    mergedAt: pr.merged_at ? new Date(pr.merged_at) : null,
     labels: pr.labels,
     draft: pr.draft,
     reviews: reviews.map((r) => ({
+      userId: r.user.id,
       user: r.user.login,
       state: r.state,
-      submittedAt: r.submitted_at,
+      submittedAt: new Date(r.submitted_at),
     })),
   };
 }
+
+export type MappedPullRequest = {
+  id: number;
+  url: string;
+  title: string;
+  state: string;
+  userId: number;
+  user: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+  closedAt: Date | null;
+  mergedAt: Date | null;
+  labels: string[];
+  draft: boolean;
+  reviews: MappedReview[];
+};
+
+export type MappedReview = {
+  userId: number;
+  user: string;
+  state: string;
+  submittedAt: Date;
+};
